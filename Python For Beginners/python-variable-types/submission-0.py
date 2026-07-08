5d4746e4852cc18690852f7da03a2b5afd392a71
Priyapondra <157760010+Priyapondra@users.noreply.github.com>
2026-07-08 11:57:07 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type=27
+float_type=27.6
+bool_type=True
+string_type="Priya"
+list_type=[1,2,3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
